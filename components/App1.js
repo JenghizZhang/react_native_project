2862c53fed175019bdbdb628a78ba7f9c17849ce
Yunsong Zhang <yunsong@gatech.edu>
2022-07-12 18:46:57 -0400

    view&StyleShee&ScrollView

--- components/App1.js
+++ components/App1.js
@@ -1,11 +1,27 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 export default function App1() {
-  return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-          <Text>
-              这是一个react native的文本元素
-          </Text>
-      </View>
-  )
+    return (
+        <View style={styles.viewContainer}>
+            <Text style={styles.text}>
+                这是一个react native的文本元素
+            </Text>
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    text: {
+        color1: 'red',
+        backgroundColor: "yellow",
+        borderColor: 'green',
+        borderWidth: 10,
+        textAlign: 'center',
+    },
+    viewContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "pink",
+    }
+})
--- components/App2.js
+++ components/App2.js
@@ -1,27 +1,22 @@
-// 2、 View组件  初步认识ScrollView组件
-import React, { Component } from 'react'
-import { View, Text, StyleSheet, ScrollView } from 'react-native'
+import React from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 
-export default class App2 extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                {/* View组件默认情况下超出的内容不可见 */}
-                {/* 把ScrollView加载需要出现滚动条的盒子内部， 默认出现的是竖向滚动条 */}
-                <ScrollView horizontal>
-                    <Text>
+export default function App2() {
+    return (
+        <View style={styles.container}>
+            <ScrollView >
+                <Text>
                     你好！！！你好！！！你好！！！你好！！！你好！！！你好！！！你好！！！你好！！！你好！！！你好！！！你好！！！你好！！！你好！！！你好！！！你好！！！你好！！！你好！！！你好！！！你好！！！你好！！！你好！！！33333
-                    </Text>    
-                </ScrollView>
-            </View>
-        )
-    }
+                </Text>
+            </ScrollView>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-    container:{
-        width:100,
-        height:100,
+    container: {
+        width: 100,
+        height: 100,
         backgroundColor: "pink"
     }
 })
