701479912b06d8b4ebf10d68772c1cee47545603
Yunsong Zhang <yunsong@gatech.edu>
2022-07-12 20:18:11 -0400

    全屏&线

--- components/App4.js
+++ components/App4.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { View, Text, Dimensions, StyleSheet } from 'react-native'
+
+const { width, height, scale } = Dimensions.get('window')
 
 export default function App4() {
-  return (
-    <div>App4</div>
-  )
+    return (
+        <View style={styles.viewContainer}>
+            <Text>屏幕的宽度是：{width}</Text>
+            <Text>屏幕的高度是：{height}</Text>
+            <Text>屏幕的像素比是：{scale}</Text>
+            <View style={{ height: 1/scale, backgroundColor: "green" }}></View>
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    viewContainer: {
+        backgroundColor: "pink",
+        flex: 1
+    }
+})
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './components/App3';
+import App from './components/App4';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
