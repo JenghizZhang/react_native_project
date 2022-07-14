c56ca038b0b52302f7f9d5fe12c53763b7b75bb0
Yunsong Zhang <yunsong@gatech.edu>
2022-07-13 22:56:24 -0400

    轮播图

--- components_day3/App5.js
+++ components_day3/App5.js
@@ -1,31 +1,32 @@
 // 5、ScrollView组件的使用
 
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, ScrollView} from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
 
 export default class App5 extends Component {
     render() {
         return (
             <View>
                 <ScrollView
-                    horizontal = {true}
-                    pagingEnabled = {true}
-                    showsHorizontalScrollIndicator = {false}
+                    horizontal={true}
+                    // pagingEnabled={true}
+                    // showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={styles.box}
                 >
                     <View style={styles.box}><Text>1</Text></View>
-                    <View style={[styles.box,styles.box2]}><Text>2</Text></View>
+                    <View style={[styles.box, styles.box2]}><Text>2</Text></View>
                 </ScrollView>
             </View>
         );
     }
 }
 const styles = StyleSheet.create({
-    box:{
-        width:360,
-        height:200,
-        backgroundColor:'#ccc',
+    box: {
+        width: "100%",
+        height: 200,
+        backgroundColor: '#ccc',
     },
-    box2:{
-        backgroundColor:'#fcf',
+    box2: {
+        backgroundColor: '#fcf',
     },
 });
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './components_day3/App5';
+import App from './components_day3/App6';
 // import App from './App';
 import {name as appName} from './app.json';
 
